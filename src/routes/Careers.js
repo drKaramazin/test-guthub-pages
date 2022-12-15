79d5fcb256da4c12e5d55b9c93346ec5748d7876
n.grishkin <din.nick@bk.ru>
2022-12-15 17:18:41 +0500

    pages

---
+++ src/routes/Careers.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Careers() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Careers
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Careers;
